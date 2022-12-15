8db3ffda2b5935fe52d6f5cbb0d83113cda67cab
orremuleisaac <oopisaac24@gmail.com>
2022-12-15 10:27:50 -0800

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 
-viod positive_or_negative(int i);
+voi:d positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-viod print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 int convert_dy(int month, int day);
 
 #endif
